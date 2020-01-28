57eb8db5c3fc4abafebba31f19efd03d0489ebf5
CodewJoy <yichen.hung6699@gmail.com>
2020-01-28 12:05:22 +0800

    install webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import './style.css'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, JSX
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App/>, document.querySelector("#root"))
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    // 當 webpack 包裝 js 檔案，採用 babel-loader 來做編譯
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
